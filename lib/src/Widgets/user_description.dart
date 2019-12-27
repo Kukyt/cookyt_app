e698d6b51158b76bbfdbcde378471807813c4a4e
JasterV <victorcoder2@gmail.com>
2019-12-27 16:58:53 +0100

    'some problems solved'

--- lib/src/Widgets/user_description.dart
+++ lib/src/Widgets/user_description.dart
@@ -1,6 +1,6 @@
 import 'package:cookyt_app/src/styles/profile_page_styles.dart';
-import 'package:flutter/material.dart';
-
+import 'package:flutter/material.dart'
+;
 class DescriptionTextWidget extends StatefulWidget {
   final String text;
 
